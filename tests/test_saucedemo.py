1c9e3d1ccd74c60865b57b181b005c520067d08c
1Alexis1002 <abarczuk@fi.uba.ar>
2025-10-24 16:33:37 -0300

    Add cart functionality test

--- tests/test_saucedemo.py
+++ tests/test_saucedemo.py
@@ -1,3 +1,4 @@
+from time import sleep
 import pytest
 import sys
 import os
@@ -16,8 +17,8 @@ def test_login(driver):
     
     login_saucedemo(driver)
     assert "/inventory.html" in driver.current_url
-    titulo = driver.find_element(By.CSS_SELECTOR, 'div.header_secondary_container .title').text
-    assert titulo== "Products"
+    title = driver.find_element(By.CSS_SELECTOR, 'div.header_secondary_container .title').text
+    assert title== "Products"
     
 def test_catalogo(driver):
     login_saucedemo(driver)
@@ -25,11 +26,28 @@ def test_catalogo(driver):
     products= driver.find_elements(By.CLASS_NAME, 'inventory_item')
     assert len(products) > 0
     
-    primer_producto= products[0]
+    primer_product= products[0]
     
-    titulo= primer_producto.find_element(By.CLASS_NAME, 'inventory_item_name').text
-    desc= primer_producto.find_element(By.CLASS_NAME, 'inventory_item_desc').text
-    precio= primer_producto.find_element(By.CLASS_NAME, 'inventory_item_price').text
-    assert titulo == "Sauce Labs Backpack"
+    title= primer_product.find_element(By.CLASS_NAME, 'inventory_item_name').text
+    desc= primer_product.find_element(By.CLASS_NAME, 'inventory_item_desc').text
+    precio= primer_product.find_element(By.CLASS_NAME, 'inventory_item_price').text
+    assert title == "Sauce Labs Backpack"
     assert desc != ""
+    assert precio == "$29.99"
+    
+def test_carrito (driver):
+    login_saucedemo(driver)
+    add_button = driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack")
+    add_button.click()
+    sleep(1)
+    
+    badge = driver.find_element(By.CLASS_NAME, "shopping_cart_badge").text
+    assert badge == "1"
+
+    driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
+
+    cart_item = driver.find_element(By.CLASS_NAME, "cart_item")
+    nombre = cart_item.find_element(By.CLASS_NAME, "inventory_item_name").text
+    precio = cart_item.find_element(By.CLASS_NAME, "inventory_item_price").text
+    assert nombre == "Sauce Labs Backpack"
     assert precio == "$29.99"
